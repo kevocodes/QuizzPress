77055ca6234757322b45c09fbcd6d20aa6bb8c66
KevinEsc21 <00116120@uca.edu.sv>
2023-05-06 04:15:00 -0600

    :pencil2: Fix: typo in the info component

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,7 +3,7 @@ import { container } from './Info.module.css'
 const Info = ({ settings, countWinners }) => {
   return (
     <div className={container}>
-      <p>Botones totales: {settings.buttonNumbers}</p>
+      <p>Botones: {settings.buttonNumbers}</p>
       <p>Seleccionados totales: {settings.winnersNumber}</p>
       <p>Por seleccionar: {settings.winnersNumber - countWinners}</p>
     </div>
